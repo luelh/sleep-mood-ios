f4efe21849de066f87d345ff53aa1733e27191fa
luelh <luelh@asleep.ai>
2025-04-04 19:01:28 +0900

    💄 NavigationBar 높이 UI 오류 수정

--- sleep-mood-ios/Application/MainTabView.swift
+++ sleep-mood-ios/Application/MainTabView.swift
@@ -3,20 +3,26 @@ import SwiftUI
 struct MainTabView: View {
     var body: some View {
         TabView {
-            MoodLightView()
-                .tabItem {
-                    Label("무드등", systemImage: "lightbulb.fill")
-                }
-            
-            SleepAnalysisView()
-                .tabItem {
-                    Label("수면분석", systemImage: "bed.double.fill")
-                }
-            
-            SettingsView()
-                .tabItem {
-                    Label("설정", systemImage: "gear")
-                }
+            NavigationView {
+                MoodLightView()
+            }
+            .tabItem {
+                Label("무드등", systemImage: "lightbulb.fill")
+            }
+
+            NavigationView {
+                SleepAnalysisView()
+            }
+            .tabItem {
+                Label("수면분석", systemImage: "bed.double.fill")
+            }
+
+            NavigationView {
+                SettingsView()
+            }
+            .tabItem {
+                Label("설정", systemImage: "gear")
+            }
         }
     }
-} 
+}
---
+++ sleep-mood-ios/Features/SleepAnalysis/Views/SleepAnalysisDetailView.swift
@@ -0,0 +1,94 @@
+//
+//  SleepAnalysisDetailView.swift
+//  sleep-mood-ios
+//
+//  Created by luelh on 4/4/25.
+//
+
+import SwiftUI
+import AsleepSDK
+
+struct SleepAnalysisDetailView: View {
+    let report: Asleep.Model.Report?
+    
+    var body: some View {
+        if let report = self.report {
+            ScrollView {
+                detailView(report: report)
+                    .frame(maxWidth: .infinity)
+            }
+        }
+    }
+    
+    @ViewBuilder
+    func detailView(report: Asleep.Model.Report) -> some View {
+        VStack(alignment: .leading, spacing: 10) {
+            Text(report.session.id)
+                .font(.title2.bold())
+            Text("Created Timezone: \(report.session.createdTimezone.description)")
+            Text("Time Range: \(report.session.startTime.fullDateString + (report.session.endTime.map { " ~ " + $0.fullDateString } ?? ""))")
+            Text("Unexpected End Time: \(report.session.unexpectedEndTime?.description ?? "N/A")")
+            Text("Session State: \(report.session.state.rawValue)")
+            Text("Missing Data Ratio: \(String(report.missingDataRatio))")
+            Text("Preculiarities: \(report.peculiarities.description)")
+            stagesView(report: report)
+        }
+        .padding(.horizontal)
+    }
+    
+    @ViewBuilder
+    func stagesView(report: Asleep.Model.Report) -> some View {
+        
+        VStack(alignment: .leading, spacing: 10) {
+            Text("Sleep Stages")
+                .font(.body.bold())
+                .padding(.top, 4)
+            if let sleepStages = report.session.sleepStages {
+                Text("[\(sleepStages.map(String.init).joined(separator: ", "))]")
+            }
+            
+            if report.stat != nil {
+                Text("""
+                    SleepEfficiency: \(report.stat?.sleepEfficiency?.description ?? "nil")
+                    SleepLatency: \(report.stat?.sleepLatency?.description ?? "nil")
+                    WakeupLatency: \(report.stat?.wakeupLatency?.description ?? "nil")
+                    SleepTime: \(report.stat?.sleepTime?.description ?? "nil")
+                    WakeTime: \(report.stat?.wakeTime?.description ?? "nil")
+                    LightLatency: \(report.stat?.lightLatency?.description ?? "nil")
+                    DeepLatency: \(report.stat?.deepLatency?.description ?? "nil")
+                    RemLatency: \(report.stat?.remLatency?.description ?? "nil")
+                    TimeInWake: \(report.stat?.timeInWake?.description ?? "nil")
+                    TimeInSleep: \(report.stat?.timeInSleep?.description ?? "nil")
+                    TimeInBed: \(report.stat?.timeInBed?.description ?? "nil")
+                    TimeInSleepPeriod: \(report.stat?.timeInSleepPeriod?.description ?? "nil")
+                    TimeInREM: \(report.stat?.timeInRem?.description ?? "nil")
+                    TimeInLight: \(report.stat?.timeInLight?.description ?? "nil")
+                    TimeInDeep: \(report.stat?.timeInDeep?.description ?? "nil")
+                    WakeRatio: \(report.stat?.wakeRatio?.description ?? "nil")
+                    SleepRatio: \(report.stat?.sleepRatio?.description ?? "nil")
+                    RemRatio: \(report.stat?.remRatio?.description ?? "nil")
+                    LightRatio: \(report.stat?.lightRatio?.description ?? "nil")
+                    DeepRatio: \(report.stat?.deepRatio?.description ?? "nil")
+                    """)
+            } else {
+                Text("Stat is nil")
+            }
+            
+            Text("Snoring Stages")
+                .font(.body.bold())
+                .padding(.top, 4)
+            if let snoringStages = report.session.snoringStages {
+                Text("[\(snoringStages.map(String.init).joined(separator: ", "))]")
+            }
+            if report.stat != nil {
+                Text("""
+                    TimeInSnoring: \(report.stat?.timeInSnoring?.description ?? "nil")
+                    TimeInNoSnoring: \(report.stat?.timeInNoSnoring?.description ?? "nil")
+                    SnoringRatio: \(report.stat?.snoringRatio?.description ?? "nil")
+                    NoSnoringRatio: \(report.stat?.noSnoringRatio?.description ?? "nil")
+                    SnoringCount: \(report.stat?.snoringCount?.description ?? "nil")
+                    """)
+            }
+        }
+    }
+}
